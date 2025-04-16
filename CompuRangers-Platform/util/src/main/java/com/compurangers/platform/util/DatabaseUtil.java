ebe85a75bc93e2df3de70f75b224518cf038c988
Pedro Gabriel Chávez <gabrielchg6@gmail.com>
2025-04-16 18:54:05 -0500

    Changed Database Managment to Singleton pattern

--- CompuRangers-Platform/util/src/main/java/com/compurangers/platform/util/DatabaseUtil.java
+++ CompuRangers-Platform/util/src/main/java/com/compurangers/platform/util/DatabaseUtil.java
@@ -1,32 +1,55 @@
 package com.compurangers.platform.util;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-    private static Connection connection = null;
-
-    public static Connection getConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            connection = DriverManager.getConnection(
-                    Config.getEnv("DB_URL"),
-                    Config.getEnv("DB_USER"),
-                    Config.getEnv("DB_PASSWORD")
-            );
+    private static DatabaseUtil dbManager;
+    private String host;
+    private int puerto;
+    private String esquema;
+    private String usuario;
+    private String password;
+    
+    private DatabaseUtil() throws IOException {
+        cargarProperties();
+    }
+    
+    public synchronized static DatabaseUtil getInstance() throws IOException {
+        if (dbManager == null) {
+            createInstance();
         }
-        return connection;
+        return dbManager;
+    }
+    
+    private static void createInstance() throws IOException {
+        dbManager = new DatabaseUtil();
+    }
+    
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String cadenaConexion = cadenaConexion(host, puerto, esquema);
+            return DriverManager.getConnection(cadenaConexion, usuario, password);
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            System.err.println(e);
+            throw e;
+        }
+    }
+    
+    private void cargarProperties() throws IOException {
+        host = Config.getEnv("DB_HOST");
+        puerto = Integer.parseInt(Config.getEnv("DB_PORT"));
+        esquema = Config.getEnv("DB_SCHEME");
+        usuario = Config.getEnv("DB_USER");
+        password = Config.getEnv("DB_PASSWORD");
+    }
+    
+    private String cadenaConexion(String host, int puerto, String esquema) {
+        return String.format("jdbc:mysql://%s:%d/%s?useSSL=false&allowPublicKeyRetrieval=true", host, puerto, esquema);
     }
 
-    public static void closeConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                connection = null;
-            }
-        }
-    }
 }
