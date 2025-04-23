5f2c499833b4263c3294717cd3bdb1860c7272d8
Drocho <gabrielchg6@gmail.com>
2025-04-22 21:24:09 -0500

    Added DAO implementation for Proveedor Class

---
+++ CompuRangers-Platform/dao/src/main/java/com/compurangers/platform/dao/mysql/inventory/ProveedorDAOImpl.java
@@ -0,0 +1,34 @@
+package com.compurangers.platform.dao.mysql.inventory;
+
+import com.compurangers.platform.core.domain.inventory.Proveedor;
+import com.compurangers.platform.dao.inventory.IProveedorDAO;
+import java.util.List;
+
+public class ProveedorDAOImpl implements IProveedorDAO{
+
+    @Override
+    public int add(Proveedor modelo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update(Proveedor modelo) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Proveedor search(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Proveedor> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
