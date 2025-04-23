2f4d6403dc4b450ad57b5a0a91ad09bc7c88f903
Drocho <gabrielchg6@gmail.com>
2025-04-22 19:14:12 -0500

    Completed models for package inventory

--- CompuRangers-Platform/core/src/main/java/com/compurangers/platform/core/domain/inventory/DocumentoCompras.java
+++ CompuRangers-Platform/core/src/main/java/com/compurangers/platform/core/domain/inventory/DocumentoCompras.java
@@ -1,5 +1,50 @@
 package com.compurangers.platform.core.domain.inventory;
 
 public class DocumentoCompras {
+    private int numero;
+    private double subtotal;
+    private double impuestos;
+    private double total;
+    private int proveedorId;
     
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getImpuestos() {
+        return impuestos;
+    }
+
+    public void setImpuestos(double impuestos) {
+        this.impuestos = impuestos;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public int getProveedorId() {
+        return proveedorId;
+    }
+
+    public void setProveedorId(int proveedorId) {
+        this.proveedorId = proveedorId;
+    }
+
 }
