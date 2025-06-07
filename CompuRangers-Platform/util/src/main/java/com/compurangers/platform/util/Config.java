ff96b39e7cb5170bed78c6a58d0355096835a98d
Drocho <gabrielchg6@gmail.com>
2025-06-07 14:55:26 -0500

    Refactor: Moved .env inside WebServices

--- CompuRangers-Platform/util/src/main/java/com/compurangers/platform/util/Config.java
+++ CompuRangers-Platform/util/src/main/java/com/compurangers/platform/util/Config.java
@@ -4,7 +4,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
     private static final Dotenv dotenv = Dotenv.configure()
-            .directory("../../")
             .load();
 
     public static String getEnv(String key) {
